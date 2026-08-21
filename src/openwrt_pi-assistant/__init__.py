9db127581360f71b025d4ec9a5a314e3e81f82a3
Thebestbmxer <etvanosdol@hotmail.com>
2026-08-20 18:54:16 -0700

    Initial Run update
    "Hello World" python script updates

--- src/openwrt_pi-assistant/__init__.py
+++ src/openwrt_pi-assistant/__init__.py
@@ -1,13 +1,1 @@
 __version__ = "0.1.0"
-Then create:
-src/openwrt_controller/main.py
-with:
-from . import __version__
-
-
-def main():
-    print(f"OpenWrt Pi Controller {__version__}")
-
-
-if __name__ == "__main__":
-    main()
