1363e55868a5b682ebe4fd124329d25b1ac40552
Thebestbmxer <etvanosdol@hotmail.com>
2026-08-20 19:11:54 -0700

    Fixing white space

--- src/openwrt_controller/main.py
+++ src/openwrt_controller/main.py
@@ -1,9 +1,7 @@
 from . import __version__
 
-
 def main():
-    print(f"OpenWrt Pi Controller {__version__}")
-
+    print(f"OpenWrt Pi Controller {__version__}")
 
 if __name__ == "__main__":
-    main()
+    main()
